652e69c53274d84bff845ff2e28bdd933f10b513
liuhao <liuhao@git.ixuenong.com>
2023-06-01 19:48:05 +0800

    orm session

---
+++ sql_orm/schema/schema.go
@@ -0,0 +1,52 @@
+package schema
+
+import (
+	"go-artisan/sql_orm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+type Schema struct {
+	Model      interface{}
+	Name       string
+	Fields     []*Field
+	FieldNames []string
+	fieldMap   map[string]*Field
+}
+
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	scheme := &Schema{
+		Model:    dest,
+		Name:     modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modelType.NumField(); i++ {
+		p := modelType.Field(i)
+		if !p.Anonymous && ast.IsExported(p.Name) {
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v, ok := p.Tag.Lookup("geeorm"); ok {
+				field.Tag = v
+			}
+			scheme.Fields = append(scheme.Fields, field)
+			scheme.FieldNames = append(scheme.FieldNames, p.Name)
+			scheme.fieldMap[p.Name] = field
+		}
+	}
+
+	return scheme
+}
